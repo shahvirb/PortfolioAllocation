c280424ff80e2af30fb7a581ce332225e444296e
shahvirb <shahvirb@gmail.com>
2021-07-22 16:00:28 -0500

    parse ofx file into UserConfig object

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import decimal
+
 import yaml
 
 
@@ -18,6 +20,19 @@ def load_yaml(path):
     return cfg
 
 
+def load_ofx(cfg):
+    for name, acct in cfg['accounts'].items():
+        if 'ofx' in acct:
+            import ofxparser
+            ofx = ofxparser.readfile(acct['ofx'])
+            statement = ofxparser.Statement(ofx, acct['ofx_accountid'])
+            cfg['accounts'][name]['holdings'] = {'securities': statement.positions()}
+            cfg['accounts'][name]['cash'] = float(
+                decimal.Decimal(cfg['accounts'][name].get('cash', 0)) + statement.cash_balance())
+            # cast the resulting cash value to a float because the user yaml file cash value is a float
+    return cfg
+
+
 class UserConfig:
     def __init__(self, path):
         self.load_yaml(path)
@@ -25,6 +40,7 @@ class UserConfig:
     def load_yaml(self, path):
         self.path = path
         self.cfg = load_yaml(path)
+        self.cfg = load_ofx(self.cfg)
 
     def __getitem__(self, item):
         return self.cfg[item]
@@ -43,3 +59,7 @@ class UserConfig:
 
     def get_portfolio(self, name):
         return self.cfg['portfolios'][name]
+
+
+if __name__ == "__main__":
+    pass
--- ofxparser.py
+++ ofxparser.py
@@ -1,16 +1,37 @@
 import ofxtools
 
+
 def readfile(path):
     parser = ofxtools.Parser.OFXTree()
     parser.parse(path)
     return parser.convert()
 
+
 def tickers_map(ofx):
     return {sec.secinfo.secid.uniqueid: sec.secinfo.ticker for sec in ofx.securities}
+
 
 def account_positions(ofx, statement_idx):
     tickers = tickers_map(ofx)
     return [(tickers[pos.invpos.secid.uniqueid], pos.invpos.units) for pos in ofx.statements[statement_idx].positions]
+
+
+class Statement:
+    def __init__(self, ofx, account_id):
+        # self.ofx = ofx
+        self.tickers_map = tickers_map(ofx)
+        self.statement = None
+        for st in ofx.statements:
+            if st.account.acctid == str(account_id):
+                self.statement = st
+        if self.statement is None:
+            raise KeyError('account_id not found in statements')
+
+    def positions(self):
+        return {self.tickers_map[pos.invpos.secid.uniqueid]: str(pos.invpos.units) for pos in self.statement.positions}
+
+    def cash_balance(self):
+        return self.statement.balances.availcash
 
 if __name__ == "__main__":
     ofx = readfile('OfxDownload.qfx')
